bf3a65250f082007a1d119e0f8553a8cde449fbd
Jose Trejos <jtrejos506@gmail.com>
2026-04-25 19:34:16 -0600

    Add qrcode package and refactor code

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,15 @@
 export const EMAIL = "jtrejos506@gmail.com";
 export const URL_GITHUB = "https://github.com/jtrejos506";
 export const URL_LINKEDIN = "https://www.linkedin.com/in/jose-506";
+export const TOOLS = {
+  REACT: "React",
+  TAILWIND: "Tailwind",
+  NODE: "Node.js",
+  POSTGRESQL: "PostgreSQL",
+  MONGODB: "MongoDB",
+  HTML: "HTML",
+  CSS: "CSS",
+  TYPESCRIPT: "TypeScript",
+  EXPRESS: "Express",
+  AWS: "AWS",
+};
